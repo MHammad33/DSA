d540d8168c97deb87fdc4f20944e2aa69bc7cdf9
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-05-15 23:41:16 +0500

    Big O

---
+++ src/Big_O/Big_O_of_n.java
@@ -0,0 +1,9 @@
+package Big_O;
+
+public class Big_O_of_n {
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Big_O/Main.java
@@ -0,0 +1,10 @@
+package Big_O;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * @dev O(n) Time Complexity explanation
+         */
+        Big_O_of_n.printItems(10);
+    }
+}
