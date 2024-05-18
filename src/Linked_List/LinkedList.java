51d6c1022cdccbdcaf10a4be8554e64cbb35e167
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-05-18 21:14:51 +0500

    Print List

--- src/Linked_List/LinkedList.java
+++ src/Linked_List/LinkedList.java
@@ -16,6 +16,32 @@ public class LinkedList {
     }
 
     /*
+     * @dev Print the values of Linked List
+     */
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    /*
+     * @dev Get the values of head and tail nodes and length of linked list
+     */
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    /*
      * @dev Creates a new node
      * Inner or Nested Class
      */
